1f46b19297468d2bd47c021223a6da1ffceebb29
MarcDietmann <marc-christoph.dietmann@capgemini.com>
2024-01-09 10:56:04 +0100

    add files

---
+++ android/app/src/main/kotlin/com/Marc/Dietmann/flutter_tdd_clean_architechture/flutter_tdd_clean_architechture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.Marc.Dietmann.flutter_tdd_clean_architechture.flutter_tdd_clean_architechture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
